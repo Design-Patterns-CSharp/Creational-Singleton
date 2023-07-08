8332c31ff4741306077446985a43a1384751e85a
payman nosraty <paymannosraty@gmail.com>
2023-07-08 11:24:28 +0330

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Singleton;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var server1 = Server.GetInstance();
+		var server2 = Server.GetInstance();
+
+		for (int index = 0; index < 10; index++)
+		{
+			Console.WriteLine(server1.GetServer());
+			Console.WriteLine(server2.GetServer());
+		}
+	}
+}
+
+
+/*
+	هنگامی که میخواهیم از یک کلاس در سطح برنامه
+	تنها یک شیء ساخته شود
+	سازنده کلاس را 
+	private
+	تعریف میکنیم
+	و یک متد با نام 
+	GetInstance
+	به صورت 
+	public
+	در کلاس تعریف کرده و برای شی سازی از این متد استفاده میکنم
+ */
---
+++ Singleton/Server.cs
@@ -0,0 +1,38 @@
+﻿namespace Singleton;
+
+public class Server
+{
+	private static Server _instance;
+	private readonly List<string> _servers = new List<string>();
+	private int _nextServer = 0;
+
+	private Server()
+	{
+		_servers.Add("192.168.1.1");
+		_servers.Add("192.168.1.2");
+		_servers.Add("192.168.1.3");
+		_servers.Add("192.168.1.4");
+	}
+
+	public static Server GetInstance()
+	{
+		_instance ??= new Server();
+
+		return _instance;
+	}
+
+	public string GetServer()
+	{
+		var result =
+			_servers[_nextServer];
+
+		_nextServer++;
+
+		if (_nextServer == _servers.Count)
+		{
+			_nextServer = 0;
+		}
+
+		return result;
+	}
+}
